04251db2b9b65f410f62ccf1e67209d396816ca1
Varga Zoltan <39100336+Lagosa@users.noreply.github.com>
2021-12-09 16:23:29 +0200

    Some more attempts

--- src/main/java/com/lagosa/HomeManager/datasource/PostgresDatasource.java
+++ src/main/java/com/lagosa/HomeManager/datasource/PostgresDatasource.java
@@ -1,26 +1,34 @@
 package com.lagosa.HomeManager.datasource;
 
 import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class PostgresDatasource {
 
-    @Autowired
-    DataSourceProperties dataSourceProperties;
+//    @Bean
+//    @ConfigurationProperties("app.datasource")
+//    public HikariDataSource hikariDataSource() {
+//        return DataSourceBuilder
+//                .create()
+//                .type(HikariDataSource.class)
+//                .driverClassName(dataSourceProperties.getDriverClassName())
+//                .build();
+//    }
+//    @Bean
+//    @Primary
+//    @ConfigurationProperties("app.datasource")
+//    public DataSourceProperties dataSourceProperties() {
+//        return new DataSourceProperties();
+//    }
 
-    @Bean
-    @ConfigurationProperties("app.datasource")
-    public HikariDataSource hikariDataSource() {
-        return DataSourceBuilder
-                .create()
-                .type(HikariDataSource.class)
-                .driverClassName(dataSourceProperties.getDriverClassName())
-                .build();
-    }
+        @Bean
+        @ConfigurationProperties("app.datasource")
+        public HikariDataSource dataSource(DataSourceProperties properties) {
+            return properties.initializeDataSourceBuilder().type(HikariDataSource.class)
+                    .build();
+        }
 }
