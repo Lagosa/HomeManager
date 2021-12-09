6ef7a187292a00b68ad0b6db1f1f364f63be4058
Varga Zoltan <39100336+Lagosa@users.noreply.github.com>
2021-12-09 16:13:27 +0200

    another attempt

--- src/main/java/com/lagosa/HomeManager/datasource/PostgresDatasource.java
+++ src/main/java/com/lagosa/HomeManager/datasource/PostgresDatasource.java
@@ -1,6 +1,8 @@
 package com.lagosa.HomeManager.datasource;
 
 import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
@@ -9,12 +11,16 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class PostgresDatasource {
 
+    @Autowired
+    DataSourceProperties dataSourceProperties;
+
     @Bean
     @ConfigurationProperties("app.datasource")
     public HikariDataSource hikariDataSource() {
         return DataSourceBuilder
                 .create()
                 .type(HikariDataSource.class)
+                .driverClassName(dataSourceProperties.getDriverClassName())
                 .build();
     }
 }
